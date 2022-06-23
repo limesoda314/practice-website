edd0788ab071a69e68c86468dfedbeb2d79b12a1
limesoda314 <amiya017@ucr.edu>
2022-06-23 06:42:47 -0700

    commiting some basic codeeeee

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	let name = 'world';
+
+	let count = 0;
+	function incrementCount() {
+		count += 1;
+	}
+
+	let llama_image = "../images/baby_llama.png";
+
+</script>
+
+
+<header> 
+	<title>Practice site</title>
+
+</header>
+<body>
+
+<div class="container">
+	<h1>Hello {name}!</h1>
+
+	<button on:click={incrementCount}>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	
+	</button>
+	
+	<hr> 
+	
+	<p1>Hello hello!</p1>
+
+	<hr>
+
+	<img src={llama_image} alt="minecraft baby llama">
+	
+</div>
+
+</body>
